cd98d1b778f099b5b3e8d4bd41a3b1a26b70f13f
Gabi <gabi@vinturing.com>
2024-06-08 17:36:26 -0300

    Iniciando o projeto, primeiros arquivos

---
+++ Clientes/Clientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clientes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Clientes");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Cliente {id}");
+        }
+
+        [HttpGet("/nome/{nome}")]
+        public IActionResult GetByNome(char nome)
+        {
+            return Ok($"Cliente {nome}");
+        }
+
+        [HttpGet("/email/{email}")]
+        public IActionResult GetByEmail(char email)
+        {
+            return Ok($"Cliente {email}");
+        }
+
+        [HttpGet("/CPF/{CPF}")]
+        public IActionResult GetByCPF(char CPF)
+        {
+            return Ok($"Cliente {CPF}");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok($"Cliente {id} atualizado");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Cliente {id} deletado");
+        }
+    }
+}
---
+++ Clientes/Clientes.API/Controllers/PedidoController.cs
@@ -0,0 +1,46 @@
+﻿using APIClientes.Entities;
+using APIClientes.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Pedido> Get()
+        {
+            List<Pedido> lista = new PedidoRepository().ObterLista();
+
+            return lista;
+        }
+
+        [HttpGet("{id}")]
+        public Pedido GetById(int id) => new PedidoRepository().ObterPorId(id);
+
+        [HttpGet("{id}/detalhado")]
+        public Pedido GetPedidoDetalhado(int id) => new PedidoRepository().ObterPedidoCompleto(id);
+
+        [HttpPost]
+        public Pedido Insert(Pedido pedido) => new PedidoRepository().Inserir(pedido);
+
+
+        /*
+        [HttpPost]
+        public Pedido Insert(Pedido pedido)
+        {
+            
+            return pedido;
+        }
+
+        [HttpPut]
+        public Pedido Update(Pedido pedido)
+        {
+            
+            return pedido;
+        }
+        */
+    }
+}
